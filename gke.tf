370fb2c131c291ce6e95be2d4a4bb19f29ab651c
Kamesh Sampath <kamesh.sampath@hotmail.com>
2023-01-25 13:52:37 +0530

    remove zone attribute from gke resource

--- gke.tf
+++ gke.tf
@@ -23,10 +23,6 @@ resource "google_container_cluster" "primary" {
   location           = local.google_zone
   min_master_version = data.google_container_engine_versions.supported.latest_master_version
 
-  node_locations = [
-    local.google_zone,
-  ]
-
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
